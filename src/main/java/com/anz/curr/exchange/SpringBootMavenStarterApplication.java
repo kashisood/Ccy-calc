e236a278a10cf4d5b4bb987f2a7955c71b16c3ea
Krishma <krishma.sood@gmail.com>
2019-03-13 09:30:59 +0530

    added log4j2

--- src/main/java/com/anz/curr/exchange/SpringBootMavenStarterApplication.java
+++ src/main/java/com/anz/curr/exchange/SpringBootMavenStarterApplication.java
@@ -1,6 +1,8 @@
 package com.anz.curr.exchange;
 
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,20 +16,27 @@ import com.anz.curr.exchange.springcore.App;
 @EnableCaching
 public class SpringBootMavenStarterApplication implements CommandLineRunner {
 
-	private static Logger LOG = LoggerFactory.getLogger(SpringBootMavenStarterApplication.class);
+	//private static Logger LOG = LoggerFactory.getLogger(SpringBootMavenStarterApplication.class);
+	private static final Logger logger = LogManager.getLogger(SpringBootMavenStarterApplication.class);
 
+	
 	@Autowired
 	private App app;
 
 	public static void main(String[] args) {
-		LOG.info("STARTING THE APPLICATION");
+		logger.info("STARTING THE APPLICATION");
 		SpringApplication.run(SpringBootMavenStarterApplication.class, args);
-		LOG.info("APPLICATION FINISHED");
+		logger.info("APPLICATION FINISHED");
 	}
 
 	@Override
 	public void run(String... args) {
-		LOG.info("EXECUTING : command line runner");
+		  logger.debug("Debugging log");
+	        logger.info("Info log");
+	        logger.warn("Hey, This is a warning!");
+	        logger.error("Oops! We have an Error. OK");
+	        logger.fatal("Damn! Fatal error. Please fix me.");
+	        logger.info("EXECUTING : command line runner");
 		app.calculate();
 	}
 }
